24498ca76b965ef6379d973a325f7b59400beac8
bqminh <64219602+BuiQuangMinh30@users.noreply.github.com>
2022-05-17 19:19:54 +0700

    first project

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Header.css'
+import  MenuIcon  from '@material-ui/icons/Menu';
+import {IconButton} from '@material-ui/core'
+import fontImage from '../images/forms_2020q4_48dp.png'
+import SearchIcon from '@material-ui/icons/Search'
+import AppsIcon from '@material-ui/icons/Apps'
+import Avatar from '@material-ui/core/Avatar'
+import avatar from '../images/avatar.jpg'
+import TemporaryDrawer from './TemporaryDrawer'
+
+function Header() {
+  return (
+    <div className="header">
+        <div className='header_info'>
+          
+            <TemporaryDrawer/>
+            <img src={fontImage}  alt="no image" className='font_image'/>
+            <div className="info">
+              Forms
+            </div>
+        </div>
+        <div className='header_search'>
+        <IconButton>
+        <SearchIcon/>
+          </IconButton>
+          
+          <input type="text" name="search" placeholder='Search'/>
+
+        </div>
+        <div className='header_right'>
+         <IconButton>
+            <AppsIcon/>
+          </IconButton>
+          <IconButton>
+            <Avatar src={avatar}/>
+          </IconButton>
+        </div>
+    </div>
+  );
+}
+
+export default Header;
